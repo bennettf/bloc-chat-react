046d5cc5b018de7709e4ddeb10d948af3fae2f24
Bennett Fong <bennettfong@mac.com>
2018-07-15 13:13:51 -0700

    list-messages-completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList.js';
+import MessageList from './components/MessageList.js';
 
 var config = {
  apiKey: "AIzaSyCD-UBqHzBZ6vMs4DEcGjLcPG1xY7gjBfA",
@@ -14,10 +15,34 @@ var config = {
 firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      activeRoom:''
+    }
+    this.changeActiveRoom=this.changeActiveRoom.bind(this);
+  }
+
+  changeActiveRoom(room){
+    this.setState({activeRoom:room})
+  }
+
   render() {
     return (
       <div className="App">
-        <RoomList firebase={firebase}/>
+        <header>
+          <h1>Bloc Chat</h1>
+        </header>
+          <section id = "sidebar">
+        <RoomList
+          firebase={firebase}
+          activeRoom={this.state.activeRoom}
+          changeActiveRoom={this.changeActiveRoom}/>
+          </section>
+        <MessageList
+          firebase={firebase}
+          activeRoom={this.state.activeRoom}/>
       </div>
     );
   }
